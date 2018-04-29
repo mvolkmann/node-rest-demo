89ef244aa14f7b9e40c6ca816e681cf594ddf2ec
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-04-28 22:08:58 -0500

    more code cleanup

--- server/src/people-router.js
+++ server/src/people-router.js
@@ -3,25 +3,13 @@
 import express from 'express';
 const PgConnection = require('postgresql-easy');
 
-const config = {database: 'demo'};
-const pg = new PgConnection(config);
-
-import {errorHandler} from './util/error-util';
+import {wrap} from './util/error-util';
 import {castObject} from './util/flow-util';
 
-type HandlerType = (
-  req: express$Request,
-  res: express$Response,
-  next: express$NextFunction
-) => Promise<mixed>;
+import {type PersonType, validatePerson} from './people';
 
-type PersonType = {
-  age: number,
-  enabled: boolean,
-  id: number,
-  firstName: string,
-  lastName: string
-};
+const config = {database: 'demo'};
+const pg = new PgConnection(config);
 
 // This maps URLs to handler functions.
 const router = express.Router();
@@ -34,11 +22,8 @@ router.post('/', wrap(postPerson));
 router.put('/:id/disable', wrap(disablePerson));
 router.put('/:id/enable', wrap(enablePerson));
 
-const requiredProperties = ['age', 'firstName', 'lastName'];
-
 function deletePerson(req: express$Request): Promise<void> {
-  pg.deleteById('people', req.params.id);
-  return Promise.resolve(); // allows usage with wrap function
+  return pg.deleteById('people', req.params.id);
 }
 
 async function disablePerson(
@@ -46,13 +31,7 @@ async function disablePerson(
   res: express$Response
 ): Promise<void> {
   const {id} = req.params;
-
-  const person = await pg.getById('people', id);
-  if (!person.enabled) {
-    res.status(400);
-    throw new Error('disablePerson called on already disabled person');
-  }
-
+  await validateEnabled(id, true, res);
   return pg.updateById('people', id, {enabled: false});
 }
 
@@ -61,20 +40,12 @@ async function enablePerson(
   res: express$Response
 ): Promise<void> {
   const {id} = req.params;
-
-  const person = await pg.getById('people', id);
-  if (person.enabled) {
-    res.status(400);
-    throw new Error('enablePerson called on already enabled person');
-  }
-
+  await validateEnabled(id, false, res);
   return pg.updateById('people', id, {enabled: true});
 }
 
 function getAllDisabled(): Promise<PersonType[]> {
-  return pg.query(
-    'select * from people where enabled is not true'
-  );
+  return pg.query('select * from people where enabled is not true');
 }
 
 function getAllEnabled(): Promise<PersonType[]> {
@@ -98,47 +69,17 @@ async function postPerson(req: express$Request): Promise<number> {
   return person;
 }
 
-function startsUpper(text: string): boolean {
-  return /^[A-Z]/.test(text);
-}
-
-function throwIf(condition: boolean, message: string) {
-  if (condition) throw new Error(message);
-}
-
-function validatePerson(person: PersonType): void {
-  for (const property of requiredProperties) {
-    const value = person[property];
-    throwIf(!value, property + ' is a required property');
+async function validateEnabled(
+  id: string,
+  want: boolean,
+  res: express$Response
+) {
+  const person = await pg.getById('people', id);
+  if (person.enabled !== want) {
+    res.status(400);
+    const kind = want ? 'disable' : 'enable';
+    throw new Error(`cannot ${kind} a person already ${kind}d`);
   }
-
-  throwIf(!startsUpper(person.firstName),
-    'firstName must start with an uppercase letter');
-
-  throwIf(!startsUpper(person.lastName),
-    'lastName must start with an uppercase letter');
-
-  throwIf(person.age < 0, 'age must be non-negative');
-}
-
-// This provides common error handling
-// for all the REST services defined here.
-function wrap(handler: HandlerType): HandlerType {
-  return async (
-    req: express$Request,
-    res: express$Response,
-    next: express$NextFunction
-  ) => {
-    try {
-      let result = await handler(req, res, next);
-      // Change numeric results to a string so
-      // Express won't think it is an HTTP status code.
-      if (typeof result === 'number') result = String(result);
-      res.send(result);
-    } catch (e) {
-      errorHandler(next, e);
-    }
-  };
 }
 
 export default router;
--- server/src/util/error-util.js
+++ server/src/util/error-util.js
@@ -1,9 +1,15 @@
 // @flow
 
+type HandlerType = (
+  req: express$Request,
+  res: express$Response,
+  next: express$NextFunction
+) => Promise<mixed>;
+
 const inTest = process.env.NODE_ENV === 'test';
 
 // istanbul ignore next
-export function errorHandler(next: express$NextFunction, error: Error): void {
+function errorHandler(next: express$NextFunction, error: Error): void {
   const msg = String(error.message ? error.message : error);
   logError(msg);
   next(new Error(msg)); // invokes builtin Express error handler
@@ -12,4 +18,28 @@ export function errorHandler(next: express$NextFunction, error: Error): void {
 export function logError(msg: string): void {
   //if (!inTest) console.trace();
   if (!inTest) console.error(msg);
+}
+
+export function throwIf(condition: boolean, message: string) {
+  if (condition) throw new Error(message);
+}
+
+// This provides common error handling
+// for all the REST services defined here.
+export function wrap(handler: HandlerType): HandlerType {
+  return async (
+    req: express$Request,
+    res: express$Response,
+    next: express$NextFunction
+  ) => {
+    try {
+      let result = await handler(req, res, next);
+      // Change numeric results to a string so
+      // Express won't think it is an HTTP status code.
+      if (typeof result === 'number') result = String(result);
+      res.send(result);
+    } catch (e) {
+      errorHandler(next, e);
+    }
+  };
 }
